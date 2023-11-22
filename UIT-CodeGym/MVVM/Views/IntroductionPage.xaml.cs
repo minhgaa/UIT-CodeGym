ddd21e05b0d46fbd60990295dbe24781496710f8
Minhgaa <bminh.cv@gmail.com>
2023-11-22 09:15:33 +0700

    addbackground

--- UIT-CodeGym/MVVM/Views/IntroductionPage.xaml.cs
+++ UIT-CodeGym/MVVM/Views/IntroductionPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class IntroductionPage : ContentPage
 	{
 		InitializeComponent();
 	}
+	protected override void OnSizeAllocated (double pageWidth, double pageHeight) {
+    base.OnSizeAllocated(pageWidth, pageHeight);
+    const double aspectRatio = 1600 / 1441.0; // Aspect ratio of the original image
+    backgroundImage.WidthRequest = Math.Max(pageHeight * aspectRatio, pageWidth);
+}
 }
