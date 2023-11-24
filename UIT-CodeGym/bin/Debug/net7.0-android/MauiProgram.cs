5edf26a071d02f4581e17ed7c35d7416e3388a53
Minhgaa <bminh.cv@gmail.com>
2023-11-24 09:24:06 +0700

    fixfont

---
+++ UIT-CodeGym/bin/Debug/net7.0-android/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+
+
+namespace UIT_CodeGym;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Raleway-Regular.ttf", "RalewayRegular");
+				fonts.AddFont("Raleway-ExtraBold.ttf", "RalewayExtraBold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
