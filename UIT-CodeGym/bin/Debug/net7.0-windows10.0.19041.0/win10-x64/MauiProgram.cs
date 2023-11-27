3ed97c1e4231501e97f31cbbb778b953af078746
TriNguyen090102 <22521523@gm.uit.edu.vn>
2023-11-27 07:44:19 +0700

    nth

---
+++ UIT-CodeGym/bin/Debug/net7.0-windows10.0.19041.0/win10-x64/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+
+
+namespace UIT_CodeGym;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Raleway-Regular.ttf", "RalewayRegular");
+				fonts.AddFont("Raleway-ExtraBold.ttf", "RalewayExtraBold");
+                fonts.AddFont("Raleway-SemiBold.ttf", "RalewaySemiBold");
+				fonts.AddFont("Raleway-Bold.ttf", "RalewayBold");
+            });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
